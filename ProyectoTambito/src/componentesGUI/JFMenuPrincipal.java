f6778c881174ecf0a992bca2fb4845e06b2c4aeb
Josephtellolaiza <142683026+Josephtellolaiza@users.noreply.github.com>
2023-11-14 12:54:00 -0500

    Add files via upload

---
+++ ProyectoTambito/src/componentesGUI/JFMenuPrincipal.java
@@ -0,0 +1,76 @@
+package componentesGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JEditorPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class JFMenuPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFMenuPrincipal frame = new JFMenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFMenuPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton(" GESTIONAR INVENTARIO ");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnNewButton.setBounds(135, 94, 161, 35);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("GENERAR VENTA");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnNewButton_1.setBounds(135, 149, 161, 35);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.setLayout(null);
+		contentPane.add(btnNewButton_1);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("   Bienvenido a tienda Tambito  ");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 22));
+		lblNewLabel.setBounds(64, 30, 308, 40);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton_2 = new JButton("Cerrar sesion ");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_2.setBounds(318, 216, 106, 34);
+		contentPane.add(btnNewButton_2);
+	}
+}
